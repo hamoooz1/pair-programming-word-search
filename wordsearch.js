220d98dd52f8f785c0b1f40896ce01913da424c0
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-28 22:10:21 -0700

    made the crossword able to check words vertically

--- wordsearch.js
+++ wordsearch.js
@@ -2,7 +2,22 @@ const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+    }   
+    let verticalWord = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let words = '';
+        for (let j = 0; j < letters.length; j++) {
+            words += letters[j][i]
+        }
+        verticalWord.push(words);
     }
+    for (let i of verticalWord) {
+        if (i.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
